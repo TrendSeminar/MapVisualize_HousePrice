23469c61c4aff12c30fab5fc94d48deffdd9d1f3
syoi92 <syoi0928@gmail.com>
2020-10-19 16:46:17 +0900

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# MapVisualize_HousePrice
+# House Price Fluctuations in Seoul, South Korea
+[[Seongyong Kim](http://syoi92.github.io)], [[Rightstone](https://)]
+
+
+This project demonstrates the house price fluctuations in units of 25 administrative districts in Seoul, South Korea into an interactive [Streamlit](https://streamlit.io) app. The unit is KRW/3.3m^2 (3.3m^2 = 1 pyeong, Korean unit of land).
+
+
+![Making-of Animation](https://raw.githubusercontent.com/TrendSeminar/MapVisualize_HousePrice/main/src/streamlit-app.mp4 "Making-of Animation")
+
+
+#### How to run this demo
+```
+git clone https://github.com/TrendSeminar/MapVisualize_HousePrice.git
+cd MapVisualize_HousePrice
+pip install --upgrade streamlit pydeck pandas
+streamlit run app.py
+```
+
+
+### Reference
+Data from [Apartment Transaction Price API](https://www.data.go.kr/data/15057511/openapi.do) from Ministry of Land, Infrastructure and Transport.
--- app.py
+++ app.py
@@ -1,37 +1,53 @@
 import streamlit as st
 import pandas as pd
 import pydeck as pdk
-import cv2, json, math, time
-
+import json, math, time
 
 
 def main():
     st.title("House Price Fluctuations in Seoul, South Korea")
-    aptdeal_wide, ori_df = load_data()    
-    df = ori_df    
-
-    # initial_time_idx = 170
-    # period = "20%02d-%02d" % ((initial_time_idx+71) // 12 , (initial_time_idx+71) % 12 + 1)
-
-    time_slider = st.empty()
+    st.sidebar.markdown("## **Controller**")
+    period_slide_info = st.sidebar.empty()
+    time_slider = st.sidebar.empty()
     period_info = st.empty()
+    deck = st.empty()
+    description = st.empty()
+    animate = st.sidebar.button('animate')    
 
-    time_idx = time_slider.slider('Time Slider', 1, 177, 170, 1)
-    period = "20%02d-%02d" % ((time_idx+71) // 12 , (time_idx+71) % 12 + 1)
+    time_idx = time_slider.slider('Period index', 1, 177, 177, 1)
+    period, cur_df = update(time_idx, period_info, period_slide_info, description)
+    deck.pydeck_chart(generate_layer(period, cur_df))
+
+    if animate:
+        for x in range(1, 177):
+            if time_idx >= 176:
+                break
+            time.sleep(.005)
+            time_idx = time_slider.slider("Period index_", 1, 177, time_idx + 1, 1)
+            period, cur_df = update(time_idx, period_info, period_slide_info, description)
+            deck.pydeck_chart(generate_layer(period, cur_df))
 
     
+    st.markdown("Reference  \n*Apartment Sale Price API* from Ministry of Land, Infrastructure and Transport")
+
+
+def update(time_idx, period_info, period_slide_info, description):
+    period = "20%02d-%02d" % ((time_idx+71) // 12 , (time_idx+71) % 12 + 1)
     cur_df = prepare_data(df, aptdeal_wide, period=period)
+
+    des1 = "**Total Average in Seoul:** %4.2f 만원/평" % cur_df[period].mean()
+    des2 = "**Maximum Value Area (Price):** %s (%4.2f 만원/평)" % \
+        (cur_df['SIG_KOR_NM'][cur_df[period].idxmax()], cur_df[period].max().max())
+
     period_info.info("**Period: %s**" % period)
-    # period_info.info("**Period**: %s ||  **Maximum Value Area**: %s (%4.2f 만원/평)" % \
-    #      (period, cur_df['SIG_KOR_NM'][cur_df[period].idxmax()], cur_df[period].max().max()))
+    period_slide_info.markdown("**Period: %s**" % period)
+    description.markdown("%s  \n%s" % (des1, des2))
+    return period, cur_df
 
-    # st.write("**Maxium Value Area (average price): **%s (%4.2f 만원/평)" % \
-    #     (cur_df['SIG_ENG_NM'][cur_df[period].idxmax()], cur_df[period].max().max()))
 
-    view_state = pdk.ViewState(
-        **{"latitude": 37.5642135, "longitude": 127.0016985, "zoom": 9, "maxZoom": 14, "pitch": 40, "bearing": 0}
-        )
 
+@st.cache(show_spinner=False, allow_output_mutation=True)
+def generate_layer(period, cur_df):
     polygon_layer = pdk.Layer(
         "PolygonLayer",
         cur_df,
@@ -48,25 +64,14 @@ def main():
         auto_highlight=True,
         pickable=True,
         )   
-
-    tooltip = {"html": "<b>지역:</b> {SIG_KOR_NM} <br/><b>기간:</b> %s <br/><b>평당가격:</b> {%s}" % (str(period),str(period))}
-
-    st.pydeck_chart(pdk.Deck(
+    
+    tooltip = {"html": "<b>지역:</b> {SIG_KOR_NM} <br/><b>기간:</b> %s <br/><b>평당가격:</b> {ave_price} 만원" % str(period)}
+    cur_deck = pdk.Deck(
         polygon_layer,
         initial_view_state=view_state,
         tooltip=tooltip
-    ))
-    st.markdown("**Maximum Value Area**: %s (%4.2f 만원/평)" % \
-         (cur_df['SIG_KOR_NM'][cur_df[period].idxmax()], cur_df[period].max().max()))
-    
-    if st.button('animate'):
-        time_idx = time_slider.slider("slider", 1, 177, 1, 1)
-        for x in range(1, 177):
-            time.sleep(.2)
-            time_idx = time_slider.slider("slider", 1, 177, time_idx + 1, 1)
-            period_info.info("Period: %s" % period)
-
-
+        )
+    return cur_deck
 
 
 @st.cache(show_spinner=False, allow_output_mutation=True)
@@ -78,10 +83,10 @@ def prepare_data(df, aptdeal_wide, period='2020-09'):
         return COLOR_RANGE[i]
 
     def cal_elevation(val):
-        #return (val)
-        return val/2 + math.exp(val * 0.0013)
+        return max((val-500)/1.5, 0) + math.exp(val * 0.0011)
+        #return max((val-500)/10 + math.exp(val * 0.0003), 0)
 
-    df['SIG_CD']=df['SIG_CD'].astype(int)
+    #df['SIG_CD']=df['SIG_CD'].astype(int)
     _df = pd.merge(df, aptdeal_wide.loc[period].to_frame(),
               how = 'left',
               left_on = 'SIG_CD',
@@ -89,15 +94,16 @@ def prepare_data(df, aptdeal_wide, period='2020-09'):
              )
     _df["fill_color"] = _df[period].apply(color_scale)
     _df["elevation"] = _df[period].apply(cal_elevation)
+    _df["ave_price"] = _df[period].apply(round)
     return _df
 
 
 @st.cache(show_spinner=False, allow_output_mutation=True)
 def load_data():
-    aptdeal = pd.read_csv('./src/csv/deal_average.csv')
+    aptdeal = pd.read_csv('./sample_data/deal_average.csv')
     aptdeal_wide = aptdeal.pivot(index='period', columns='region_code', values='price_per_pyeong').fillna(method='pad').fillna(0)
 
-    with open('./src/SIG_geojson/sig_seoul.geojson') as f:
+    with open('./sample_data/sig_seoul.geojson') as f:
         sig_r = json.load(f)
     sig = pd.DataFrame(sig_r["features"])
     sig_geo, sig_prop = sig["geometry"], sig["properties"]
@@ -106,11 +112,17 @@ def load_data():
     df = pd.DataFrame()
     df["coordinates"] = sig_geo.apply(lambda row: row["coordinates"][0])
     df["SIG_CD"] = sig_prop.apply(lambda row: row["SIG_CD"])
+    df['SIG_CD']=df['SIG_CD'].astype(int)
     df["SIG_ENG_NM"] = sig_prop.apply(lambda row: row["SIG_ENG_NM"])
     df["SIG_KOR_NM"] = sig_prop.apply(lambda row: row["SIG_KOR_NM"])
-
     return aptdeal_wide, df
 
+aptdeal_wide, df = load_data() 
+view_state = pdk.ViewState(
+    **{"latitude": 37.5642135,
+     "longitude": 127.0016985,
+      "zoom": 9, "maxZoom": 14, "pitch": 60, "bearing": 0}
+    )
 COLOR_RANGE = [
     [65, 182, 196],
     [127, 205, 187],
@@ -126,7 +138,6 @@ COLOR_RANGE = [
     [189, 0, 38],
     [128, 0, 38],
 ]
-
 BREAKS = [500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000, 6500]
 
 
