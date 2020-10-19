e361e42fe7890820a9b929119c8e7bb50316a9b1
syoi92 <syoi0928@gmail.com>
2020-10-20 01:05:43 +0900

    _

--- app.py
+++ app.py
@@ -1,31 +1,35 @@
 import streamlit as st
 import pandas as pd
 import pydeck as pdk
-import json, math, time
+import math, time
 
 
 def main():
     st.title("House Price Fluctuations in Seoul, South Korea")
     st.sidebar.markdown("## **Controller**")
+
     period_slide_info = st.sidebar.empty()
     time_slider = st.sidebar.empty()
     period_info = st.empty()
     deck = st.empty()
     description = st.empty()
     animate = st.sidebar.button('animate')    
+    st.sidebar.markdown("From this period, sliding to the end")
 
-    time_idx = time_slider.slider('Period index', 1, 177, 177, 1)
+    time_idx = time_slider.slider('[2006-01 ~ 2020-09]', 1, 177, 88, 1)
     period, cur_df = update(time_idx, period_info, period_slide_info, description)
     deck.pydeck_chart(generate_layer(period, cur_df))
 
     if animate:
+        time_idx = 75 if time_idx > 170 else time_idx
         for x in range(1, 177):
-            if time_idx >= 176:
-                break
             time.sleep(.005)
-            time_idx = time_slider.slider("Period index_", 1, 177, time_idx + 1, 1)
+            time_idx = time_slider.slider("[2006-01 ~ 2020-09]", 1, 177, time_idx + 1, 1)
             period, cur_df = update(time_idx, period_info, period_slide_info, description)
             deck.pydeck_chart(generate_layer(period, cur_df))
+
+            if time_idx >= 177:
+                break
 
     
     st.markdown("Reference  \n*Apartment Transaction Price API* from Ministry of Land, Infrastructure and Transport")
@@ -97,14 +101,14 @@ def prepare_data(df, aptdeal_wide, period='2020-09'):
     _df["ave_price"] = _df[period].apply(round)
     return _df
 
-
+dd
 @st.cache(show_spinner=False, allow_output_mutation=True)
 def load_data():
-    aptdeal = pd.read_csv('./sample_data/deal_average.csv')
+    aptdeal = pd.read_csv('https://raw.githubusercontent.com/TrendSeminar/MapVisualize_HousePrice/main/sample_data/deal_average.csv')
     aptdeal_wide = aptdeal.pivot(index='period', columns='region_code', values='price_per_pyeong').fillna(method='pad').fillna(0)
 
-    with open('./sample_data/sig_seoul.geojson') as f:
-        sig_r = json.load(f)
+    import requests, json
+    sig_r = json.loads(requests.get('https://raw.githubusercontent.com/TrendSeminar/MapVisualize_HousePrice/main/sample_data/sig_seoul.geojson').text)
     sig = pd.DataFrame(sig_r["features"])
     sig_geo, sig_prop = sig["geometry"], sig["properties"]
 
